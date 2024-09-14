995983c119d75c49f98446d0239e97c19617dae8
Padoomay <padoomay@gmail.com>
2024-09-14 14:55:38 +0500

    Вывод сообщения в консоль
    Вывод сообщения в консоль каждый кадр

---
+++ Crickets.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    private void Update()
+    {
+        // Вывод сообщения в консоль каждый кадр
+        print("crickets");
+    }
+}
