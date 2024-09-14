d397fa560958531e39e3475a88f723557775b092
Padoomay <padoomay@gmail.com>
2024-09-14 14:31:44 +0500

    Add files via upload
    Вывод сообщения "crickets" в консоль каждый кадр

---
+++ Crickets.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    private void Update()
+    {
+        // Вывод сообщения "crickets" каждый кадр
+        print("crickets");
+    }
+}
